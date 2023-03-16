626d7f73484c0abd0a5173abfc997dd1df68513b
Elinmaculado <miguel.ruiz@uniat.edu.mx>
2023-03-16 10:23:10 -0600

    Very close logic

--- Guessmynumber.cpp
+++ Guessmynumber.cpp
@@ -11,6 +11,7 @@ int main()
     int secretNumber = (randomNumber % 300) + 1;
     int guess;
     int tries = 0;
+    int veryclose;
 
     cout << "GUESS MY NUMBER" << endl;
     cout << "Adivina el número en el menor número de intentos posible." << endl;
@@ -22,6 +23,12 @@ int main()
         cout << "\nEnter a guess (1-300):" << endl;
         cin >> guess;
         tries++;
+
+        veryclose = secretNumber - guess;
+
+        if (veryclose < 5 && veryclose > -5 && veryclose != 0) {
+            cout << "estas muy cerca" << endl;
+        }
 
         if (guess > secretNumber) {
             cout << "Muy Alto\n\n";
